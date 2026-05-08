f35e5441046aa8dca2384db569f864a373cc32a4
AtomicInternet2 <132714132+AtomicInternet2@users.noreply.github.com>
2026-05-08 16:36:20 -0400

    Update EventLogExport.ps1

--- EventLogExport.ps1
+++ EventLogExport.ps1
@@ -13,39 +13,10 @@ for ($num = 1 ; $num -le 5 ; $num++) {
 
 Write-Output '--------------------------- UPS -----------------------------'
 
-$Battery = Get-CimInstance -ClassName win32_battery
-Switch ($Battery.Availability) {
-    1  { $Availability = "Other" ;break}
-   2  { $Availability =  "Not using battery" ;break}
-   3  { $Availability = "Running or Full Power";break}
-   4  {$Availability =  "Warning" ;break}
-   5  { $Availability = "In Test";break}
-   6  { $Availability = "Not Applicable";break}
-   7  { $Availability = "Power Off";break}
-   8  { $Availability = "Off Line";break}
-   9  { $Availability = "Off Duty";break}
-   10  {$Availability =  "Degraded";break}
-   11  {$Availability =  "Not Installed";break}
-   12  {$Availability =  "Install Error";break}
-   13  { $Availability = "Power Save - Unknown";break}
-   14  { $Availability = "Power Save - Low Power Mode" ;break}
-   15  { $Availability = "Power Save - Standby";break}
-   16  { $Availability = "Power Cycle";break}
-   17  { $Availability = "Power Save - Warning";break}
-    }
-
-$BatteryStatus = $Battery.Status
-$BatteryName = "$($Battery.name)"
-$Remaining = $Battery.EstimatedChargeRemaining
-$EstRunTimeMinutes = $Battery.EstimatedRunTime
-$EstCharge = $Battery.EstimatedChargeRemaining
-$EstPercUsed = $Battery
-$BatAvailability = $Availability
-
-'Status: ' + $BatteryStatus
-'Name: ' + $BatteryName
-'Remaining: ' + $EstRunTimeMinutes + ' Minutes'
-'Charge: ' + $EstCharge + '%'
-'Availability: ' + $BatAvailability
+'Status: Not Connected'
+'Name: None'
+'Remaining: 0'
+'Charge: 0'
+'Availability: 0'
 
 Get-WinEvent -FilterHashTable @{Logname='System';Level=2,3} -MaxEvents 20 | Format-Table TimeCreated,ProviderName,Message -wrap
